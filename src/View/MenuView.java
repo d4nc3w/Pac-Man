551fab09d446d7e7c3ef79d3f9d969f46467faab
s28113 <s28113@pjwstk.edu.pl>
2024-04-11 22:22:50 +0200

    Adding complete Menu view

--- src/View/MenuView.java
+++ src/View/MenuView.java
@@ -12,28 +12,42 @@ public class MenuView extends JFrame {
 
     public MenuView(){
         setTitle("Pac-Man");
-        setSize(400, 200);
+        setSize(550, 330);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setResizable(false);
 
-        initiateComponents();
+        setContentPane(new JLabel(new ImageIcon("res/MenuBackground.jpg")));
+        setLayout(new BorderLayout());
+        add(createButtonPanel(), BorderLayout.CENTER);
     }
 
-    private void initiateComponents() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 1));
+    private JPanel createButtonPanel() {
+        JPanel buttonPanel = new JPanel(new GridBagLayout());
+        buttonPanel.setOpaque(false);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
 
         newGameButton = new JButton("New Game");
+        newGameButton.setPreferredSize(new Dimension(150, 50));
+        buttonPanel.add(newGameButton, gbc);
+
+        gbc.gridy++;
         highScoresButton = new JButton("High Scores");
+        highScoresButton.setPreferredSize(new Dimension(150, 50));
+        buttonPanel.add(highScoresButton, gbc);
+
+        gbc.gridy++;
         exitGameButton = new JButton("Exit");
+        exitGameButton.setPreferredSize(new Dimension(150, 50));
+        buttonPanel.add(exitGameButton, gbc);
 
-        panel.add(newGameButton);
-        panel.add(highScoresButton);
-        panel.add(exitGameButton);
-
-        add(panel);
-
+        return buttonPanel;
     }
+
     public void addNewGameButtonListener(ActionListener listener) {
         newGameButton.addActionListener(listener);
     }
