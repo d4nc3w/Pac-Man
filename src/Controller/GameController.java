fd29b8801263e05ec195af09f5c7c4b7e98c91d0
s28113 <s28113@pjwstk.edu.pl>
2024-04-12 17:38:27 +0200

    Added LevelController class containing all maps, added points render

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -2,14 +2,83 @@ package Controller;
 
 import View.GameView;
 
-public class GameController {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
+public class GameController implements KeyListener {
     private GameView view;
+    private int lives, score;
+    private boolean gameOver;
+    private boolean dying = false;
+
+    private final int BLOCK_SIZE = 20;
+    private final int NUM_OF_BLOCKS = 30;
+    private final int SCREEN_SIZE = NUM_OF_BLOCKS * BLOCK_SIZE;
+    private final int MAX_GHOSTS = 8;
+    private final int PACMAN_SPEED = 6;
+    private int NUM_OF_GHOSTS = 6;
+
+    private int[] dx, dy;
+    private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
+    private int pacman_x, pacman_y, pacman_dx, pacman_dy;
+    private int req_dx, req_dy;
+    private final int validSpeeds[] = {1,2,3,4,6,8};
+    private final int maxSpeed = 6;
+    private int currentSpeed = 3;
+    private Timer timer;
+    private final int[][] levelData = LevelController.getRandomLevel();
 
     public GameController(GameView view){
         this.view = view;
+        view.passLevelData(levelData);
+        view.addKeyListener(this);
+        initVariables();
     }
 
     public void startGame() {
         view.setVisible(true);
+    }
+
+    private void initVariables(){
+        lives = 3;
+        score = 0;
+        gameOver = false;
+
+        //TODO: Other variables...
+    }
+
+    public void updateGameState(){
+        //...
+    }
+
+    public void renderGame(){
+        //...
+    }
+
+    private void handleInput(int keyCode){
+        //...
+    }
+
+    private void gameOver(){
+        //...
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        int keyCode = e.getKeyCode();
+        handleInput(keyCode);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        //NOT USED
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //NOT USED
     }
 }
--- src/View/GameView.java
+++ src/View/GameView.java
@@ -1,12 +1,48 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameView extends JFrame {
+    private final int BLOCK_SIZE = 24;
+    private final int NUM_OF_BLOCKS = 20;
+    private final int SCREEN_SIZE = NUM_OF_BLOCKS * BLOCK_SIZE;
+    private int[][] levelData;
+
     public GameView() {
         setTitle("Pac-Man Game");
-        setSize(800, 600);
+        setSize(SCREEN_SIZE, SCREEN_SIZE);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    public void passLevelData(int[][] levelData) {
+        this.levelData = levelData;
+    }
+
+    public void paint(Graphics g) {
+        super.paint(g);
+        if (levelData != null) {
+            for (int y = 0; y < NUM_OF_BLOCKS; y++) {
+                for (int x = 0; x < NUM_OF_BLOCKS; x++) {
+                    int blockValue = levelData[y][x];
+                    if (blockValue == 1) {
+                        g.setColor(Color.BLUE);
+                        g.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                    }
+                    else if (blockValue == 2){
+                        g.setColor(Color.BLACK);
+                        g.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                    }
+                    else if (blockValue == 0){
+                        g.setColor(Color.BLACK);
+                        g.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                        g.setColor(Color.WHITE);
+                        g.fillOval(x * BLOCK_SIZE + BLOCK_SIZE / 2, y * BLOCK_SIZE + BLOCK_SIZE / 2, BLOCK_SIZE / 4, BLOCK_SIZE / 4);
+                    }
+                }
+            }
+        }
     }
 }
